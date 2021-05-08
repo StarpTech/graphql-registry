5008c4144a9955ffbe2577eca55a1dc59856b729
starptech <deusdustin@gmail.com>
2021-05-08 16:41:59 +0200

    support .env in migrations

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,10 +1,14 @@
 import { join } from 'path'
+import dotenv from 'dotenv'
+
+// allows to use .env for development because migrations must be compiled
+dotenv.config({ path: join(__dirname, '..', '.env') })
 
 export default {
   client: 'pg',
   connection: process.env.DATABASE_URL,
   migrations: {
     extension: 'ts',
-    directory: join(__dirname + '/migrations'),
+    directory: join(__dirname, '/migrations'),
   },
 }
